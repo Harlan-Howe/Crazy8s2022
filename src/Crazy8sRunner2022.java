4f37584434e0aeb6a1a23d2abc2fba0ed1e1a52e
Harlan Howe <hhowe7@gatech.edu>
2022-09-24 22:22:36 -0500

    Wrote some code to support (and encourage) testing the HumanPlayer and ComputerPlayer.

--- src/Crazy8sRunner2022.java
+++ src/Crazy8sRunner2022.java
@@ -5,8 +5,27 @@ public class Crazy8sRunner2022
 
     public static void main(String[] args)
     {
+        // use this code when you are testing out the two player classes. Comment it out when they are working correctly.
+        runTestsOnPlayers();
+        System.out.println("----------------------------------------- Starting game.");
         Referee ref = new Referee();
         ref.runGame();
+    }
+
+    public static void runTestsOnPlayers()
+    {
+        // Let's make a temporary deck of cards just for testing out our player classes.
+        CardPile practiceDeck = CardPile.makeDeck(CardPile.ACE_LOW);
+
+        System.out.println("Testing human player.");
+        HumanPlayer testHP = new HumanPlayer();
+        // add more stuff here to test out the methods you write in Human player.
+
+
+        System.out.println("Testing computer player.");
+        ComputerPlayer testCP = new ComputerPlayer();
+        // add more stuff here to test out the methods you write in computer player.
+
     }
 
 }
