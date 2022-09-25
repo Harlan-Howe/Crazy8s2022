64739acc4c2ae0d6f7aa51b3e45c9f6e3745aabd
Harlan Howe <hhowe7@gatech.edu>
2022-09-24 22:14:36 -0500

    added note on where to find documentation.

--- src/Crazy8sRunner2022.java
+++ src/Crazy8sRunner2022.java
@@ -1,5 +1,8 @@
 public class Crazy8sRunner2022
 {
+    // Note: you can find Card & CardPile documentation at https://bit.ly/CardStarterDocs or go in this project folder
+    //            to Crazy8s2022/docs/allclasses-index.html and open that in a browser.
+
     public static void main(String[] args)
     {
         Referee ref = new Referee();
